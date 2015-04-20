092bcdf8a5cbf4a5c512fab8256a4e0344f6ebfe
dongzhukai <dongzhukai@gmail.com>
2015-04-20 08:32:17 +0800

    add

--- volume1/chapter14/src/AlgorithmAnimation/AlgorithmAnimation.java
+++ volume1/chapter14/src/AlgorithmAnimation/AlgorithmAnimation.java
@@ -81,7 +81,7 @@ class Sorter implements Runnable
 {
    /**
     * Constructs a Sorter.
-    
+
     * @param comp the component on which to display the sorting progress
     */
    public Sorter(ArrayComponent comp)
